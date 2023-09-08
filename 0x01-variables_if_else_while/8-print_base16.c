ab15ae90fbfc13c51b6789ef8a9639f1f12a21e4
julietwanza <julietwanza2021@gmail.com>
2023-09-08 13:44:14 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,8 +9,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 16; num++)
-		putchar((num % 16) + '0');
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
 	putchar('\n');
 	return (0);
 }

9e660f441576b004ccb140e760d84c2b55598bd9
julietwanza <julietwanza2021@gmail.com>
2023-09-12 13:39:34 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,10 +7,10 @@
 
 void print_alphabet(void)
 {
-	char c;
+	char letter;
 
-	for (c = 'a' ; c <= 'z'; ++c);
-	_putchar(c);
+	for (letter = 'a' ; letter  <= 'z' ; letter++)
+	_putchar(letter);
 
 	_putchar('\n');
 }

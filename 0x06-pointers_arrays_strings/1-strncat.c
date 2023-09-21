d0e51b7252f903e36e99fe99258380ef64e2f16a
julietwanza <julietwanza2021@gmail.com>
2023-09-21 12:02:48 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,18 +6,22 @@
  * @dest :- first string to be concatinated with src
  * @src :- second string to be concatinated with dest
  * @n :- number of elements to concatenate in
- * Return: void
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int a;
-	int b;
 
-	for (a = 0; dest[a] != '\0'; a++)
-		;
-	for (b = 0; src[b] != '\0'; b++, n--, a++)
+	a = 0;
+	while (a < n && src[a] != '\0')
 	{
-		dest[a] = src[b];
+		dest[a] = src[a];
+		a++;
+	}
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
 	}
 	return (dest);
 }
